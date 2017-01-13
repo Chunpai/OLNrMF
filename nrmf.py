9952db94e9659359b4b60424335b127acfc4bd8e
Chunpai <chunpaiwang@gmail.com>
2017-01-13 01:26:02 -0500

    nrmf

--- nrmf.py
+++ nrmf.py
@@ -5,11 +5,11 @@ import datetime
 import matplotlib.pyplot as plt
 import random
 import readData
+from copy import deepcopy
 
 
 
-
-def initialization(n,l,r):
+def initialization(n,r,l):
     """
     initialize F and G as matrix with 0s as all entries 
     the reason to initialize F and G as matrix rather than dictionary is because they are not sparse
@@ -23,105 +23,113 @@ def initialization(n,l,r):
 
 
 
-def AltQPInc(movie_dict,user_dict, F,G,n,l,r):
+def AltQP_Inc(M1,n,r,l):
     """
     input: original n*l matrix A (here we use A_dict to represent matrix), and rank size r
     output: n*r matrix F, r*l matrix G, and an n*l matrix R
     """ 
     #R_dict =  movie_dict #residual matrix initialized as a dict due to sparsity
+    F, G = initialization(n,r,l)
+    R = deepcopy(M1)
     for k in range(r):
-        f, g = RankOneApproximation(movie_dict, user_dict,n,l,r)
+        f, g = RankOneApproximation(R,n,l)
         F[:,k] = f
         G[k,:] = g
-        for movie in movie_dict:
-            for user in movie_dict[movie]:
-                if movie_dict[movie][user] > 0.0:
-                    movie_dict[movie][user] = movie_dict[movie][user] - f[movie-1]*g[user-1]
-                    user_dict[user][movie] = user_dict[user][movie] - f[movie-1]*g[user-1]
-    return F, G, movie_dict, user_dict
+        for user_index in R:
+            for item_index in R[user_index]:
+                if R[user_index][item_index] > 0.0:
+                    R[user_index][item_index] -= f[user_index]*g[item_index]
+                else:
+                    print 'ERROR--------------------------------------------' 
+    return F, G, R
    
 
-def RankOneApproximation(movie_dict,user_dict,n,l,r):
+def RankOneApproximation(R,n,l):
     """
     input: A_dict, the matrix needs to be factorized
     output: a colunmn vector f (movie feature), and a row vector g (user feature)
     """
     convergent = False
-    f = []
-    g = []
-    for i in range(n):
-        f.append(random.randint(-5,5)/1.0)
-    for j in range(l):
-        g.append(random.randint(-5,5)/1.0)
-    f = np.array(f)
-    f = f.T
-    g = np.array(g)
-    #print "f",f
-    #print "g",g
-    product = np.outer(f,g)
-    #print "product",product
+    #f = []
+    #g = []
+    #for i in range(n):
+    #    f.append(random.randint(-5,5)/1.0)
+    #for j in range(l):
+    #    g.append(random.randint(-5,5)/1.0)
+    #f = np.array(f)
+    #f = f.T
+    #g = np.array(g)
+    f = np.random.rand(n)
+    g = np.random.rand(l)
+    print 'f',f
+    print 'g',g
+    iteration = 0
     while convergent == False:
         total_error = 0.0
-        g = Update_g(movie_dict,f,g,n,l,r)
-        #print "update-g",g
-        f_hat = Update_g(user_dict,g.T,f.T,l,n,r)  #note the order of parameters n and l
-        f = f_hat.T
-        #print "update-f",f
-        product_next = np.outer(f,g)
-        """
-        error = LA.norm(product_next - product)
-        norm = LA.norm(product_next)
-        """
-        for movie in movie_dict:
-            for user in movie_dict[movie]:
-                rating = movie_dict[movie][user]
+        iteration += 1
+        print 'ITERATION',iteration
+        g = Update_g(R,f,l)
+        print 'g updated'
+        f = Update_f(R,g,n)  #note the order of parameters n and l
+        print 'f updated'
+        P = outer_prod(f,g)
+        for user_index in R:
+            for item_index in R[user_index]:
+                rating = R[user_index][item_index] 
                 if rating > 0.0:
-                    error = product_next[movie-1][user-1] - rating 
-                    if error > 0.0:
-                        abc= "OMGGGGGGGGGGGGGGG, SOMTHING WRONG"
+                    if user_index in P and item_index in P[user_index]:
+                        approx = P[user_index][item_index]
+                        print 'approx',approx
+                        error = rating - approx
+                        print 'error1',error
+                    else:
+                        error = rating
+                        print 'error2',error
+                    if error < 0.0:
+                        print 'ERROR------------------------------------2'
+                        print 'error3',error
+                        print 'rating',rating
+                        print 'prod', P[user_index][item_index]
+                        return
                     elif error == 0.0:
-                        abc= "gooooooooooooooooooooooood"
+                        print 'Good-------------------------------------'
                     else:
                         total_error += math.pow(error,2)
-        #print "norm",norm
         total_error = math.sqrt(total_error)
-        #print "total_error",total_error
-        #print "product_next",product_next
-        if total_error > 1000.0:
-            product = product_next
-        else:
+        if total_error <= 1000.0 or iteration >= 100:
             convergent = True
-            #print "convergent------------------------------------------------------------"
+            print 'convergent-------------------------------------------'
+        else:
+            print 'total_error',total_error
     return f, g
 
 
-def Update_g(A_dict,f,g,n,l,r):
-    """
-    input: the origin matrix (movie_dict) needs to be factorized, and a column vector
-    output: a row vector
-    """
+def Update_g(R,f,l):
+    g = np.zeros(l)
     for j in range(l):
-        low = float("-inf")
-        up = float("inf")
+        low = float('-inf')
+        up = float('inf')
         t = 0.0
         q = 0.0
-        for movie in A_dict:
-            user = j+1
-            if user in A_dict[movie]:
-                if A_dict[movie][user] > 0.0:
-                    q = q + f[movie-1] * A_dict[movie][user]
-                    t = t + math.pow(f[movie-1],2)
-                    #print f[movie-1]
-                    if f[movie-1] > 0:
-                        up = min(up, A_dict[movie][user]/f[movie-1]) 
-                    elif f[movie-1] < 0:
-                        low = max(low, A_dict[movie][user]/f[movie-1])
+        for user_index in R:
+            f_value = f[user_index]
+            if j in R[user_index]:
+                if R[user_index][j] > 0.0:
+                    q = q + f_value * R[user_index][j]
+                    t = t + math.pow(f_value,2)
+                    if f_value > 0:
+                        up = min(up, R[user_index][j] / f_value) 
+                    elif f_value < 0:
+                        low = max(low, R[user_index][j] / f_value)
                     else:
                         continue
+            else:
+                continue
         if t == 0:
             g[j] = 0.0 
             continue
         q = q / t
+        q = float("{0:.2f}".format(q))
         if q <= up and q >= low:
             g[j] = q
         elif q > up:
@@ -131,27 +139,82 @@ def Update_g(A_dict,f,g,n,l,r):
     return g    
 
 
-def plotResult(R_dict,n,l):
+def Update_f(R,g,n):
+    f = np.zeros(n)
+    for i in range(n):
+        low = float('-inf')
+        up = float('inf')
+        t = 0.0
+        q = 0.0
+        if i in R:
+            for item_index in R[i]:
+                g_value = g[item_index]
+                if R[i][item_index] > 0.0:
+                    q = q + g_value * R[i][item_index]
+                    t = t + math.pow(g_value,2)
+                    if g_value > 0:
+                        up = min(up, R[i][item_index] / g_value) 
+                        up = float("{0:.2f}".format(up))
+                    elif g_value < 0:
+                        low = max(low, R[i][item_index] / g_value)
+                        low = float("{0:.2f}".format(low))
+                    else:
+                        continue
+        if t == 0:
+            f[i] = 0.0 
+            continue
+        q = q / t
+        if q <= up and q >= low:
+            f[i] = q
+        elif q > up:
+            f[i] = up
+        else:
+            f[i] = low  
+    return f 
 
+
+def outer_prod(f,g):
+    P = {}
+    n = f.size 
+    l = f.size
+    for i in range(n):
+        P[i] = {}
+        if f[i] != 0.0:
+            for j in range(l):
+                if g[j] != 0.0:
+                    P[i][j] = f[i]*g[j]
+    return P 
+             
+
+
+def plotResult(R,n,l):
     plt.axis([0,l+1,0,n+1])
     count = 0
-    for movie in R_dict:
+    for user_index in R:
         user_list = []
-        movie_list = []
-        for user in R_dict[movie]:
-            if R_dict[movie][user] > 0.00001:
+        item_list = []
+        for item_index in R[user_index]:
+            if R[user_index][item_index] > 0.0000:
                 count += 1
-                user_list.append(user)
-                movie_list.append(movie)
-        plt.plot(user_list, movie_list, "b.")
-    plt.savefig("origin.png")
+                user_list.append(user_index)
+                item_list.append(item_index)
+        plt.plot(user_list, item_list, 'b.')
+    plt.savefig('anomaly.png')
     print count
 
 
 
 
 if __name__ == "__main__": 
-    data_dir = 'datasets/Amazon/'
-    filename = 'ratings_Movies_and_TV.csv'
-    M1, M2 = readData.preprocess(data_dir, filename)
-    print M1.shape, M2.shape
+    data_dir = 'datasets/Amazon/top1000/'
+    filename = 'ratings.csv'
+
+    M1, M2 ,user_dict, item_dict = readData.loadPickle(data_dir)
+    n = len(user_dict)
+    l = len(item_dict)
+    r = 1
+    AltQP_Inc(M1,n,r,l)
+    
+
+
+
--- readData.py
+++ readData.py
@@ -17,8 +17,8 @@ def preprocess(data_dir, filename):
     item_index = -1
     user_dict = {}
     item_dict = {}
-    G1 = nx.Graph()   #before 2010
-    G2 = nx.Graph()   #after 2010
+    M1 = {}
+    M2 = {}
     for line in infile:
         fields = line.strip().split(',')
         #print fields
@@ -31,31 +31,74 @@ def preprocess(data_dir, filename):
             item_dict[item_index] = item
         year = datetime.datetime.fromtimestamp(int(timestamp)).strftime('%Y')#'%Y-%m-%d %H:%M:%S')
         if int(year) <= 2010:
-            G1.add_edge(user_index,item_index,weight=float(rating))  
-            G2.add_edge(user_index,item_index,weight=0.0)
+            if user_index not in M1:
+                M1[user_index] = {}
+                M1[user_index][item_index] = float(rating)
+            else:
+                M1[user_index][item_index] = float(rating)
         else:
-            G2.add_edge(user_index,item_index,weight=float(rating))  
-            G1.add_edge(user_index,item_index,weight=0.0)
+            if user_index not in M2:
+                M2[user_index] = {}
+                M2[user_index][item_index] = float(rating)
+            else:
+                M2[user_index][item_index] = float(rating)
     infile.close()
-    M1 = np.array(nx.to_numpy_matrix(G1)) 
-    M2 = np.array(nx.to_numpy_matrix(G2)) 
-    #dumpPickle(data_dir, M1, M2)
-    return M1, M2
+    print len(M1)
+    print len(M2)
+    dumpPickle(data_dir, M1, M2, user_dict, item_dict)
+    #return  M1, M2, user_dict, item_dict
 
 
-def dumpPickle(data_dir, M1, M2):
-    pickle.dump(M1, open('M1.pkl','wb'))
-    pickle.dump(M2, open('M2.pkl','wb'))  
+def dumpPickle(data_dir, M1, M2, user_dict, item_dict):
+    pickle.dump(M1, open(data_dir+'M1.pkl','wb'))
+    pickle.dump(M2, open(data_dir+'M2.pkl','wb'))  
+    pickle.dump(user_dict, open(data_dir+'user_dict.pkl','wb'))
+    pickle.dump(item_dict, open(data_dir+'item_dict.pkl','wb'))
  
 
 def loadPickle(data_dir):
-    M1 = pickle.load(open('M1.pkl','rb'))
-    M2 = pickle.load(open('M2.pkl','rb'))
-    return M1, M2
+    M1 = pickle.load(open(data_dir+'M1.pkl','rb'))
+    M2 = pickle.load(open(data_dir+'M2.pkl','rb'))
+    user_dict = pickle.load(open(data_dir+'user_dict.pkl','rb'))
+    item_dict = pickle.load(open(data_dir+'item_dict.pkl','rb'))
+    return M1, M2, user_dict, item_dict
+
+
+def extractTop1000(data_dir,infilename,outfilename):
+    '''
+    extract rating records of first 1000 users, and save as /top1000/ratings.csv
+    '''
+    infile = open(data_dir+infilename,'r') 
+    first = infile.readline()
+    outfile = open(data_dir+outfilename,'w')
+    outfile.write(first)
+    user_index = -1
+    item_index = -1
+    user_dict = {}
+    item_dict = {}
+    M1 = {}
+    M2 = {}
+    for line in infile:
+        fields = line.strip().split(',')
+        #print fields
+        [user,item,rating,timestamp] = fields
+        if user not in user_dict and user_index < 999:
+            user_index += 1
+            user_dict[user] = user_index
+            outfile.write(line)
+        elif user in user_dict:
+            outfile.write(line)
+    print len(user_dict)
+    infile.close()
+    outfile.close() 
 
 
 
 if __name__ == '__main__':
-    data_dir = 'datasets/Amazon/'
-    filename = 'ratings_Movies_and_TV.csv'
-    preprocess(data_dir,filename)
+    #data_dir = 'datasets/Amazon/'
+    #infilename = 'ratings_Movies_and_TV.csv'
+    
+    data_dir = 'datasets/Amazon/top1000/'
+    infilename = 'ratings.csv'
+    preprocess(data_dir,infilename)
+    #extractTop1000('datasets/Amazon/','ratings_Movies_and_TV.csv','top1000/ratings.csv')
